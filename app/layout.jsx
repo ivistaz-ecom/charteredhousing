0fbe05ca87c43eb90ffbd2e254da3539b64283b6
Shubham Shree <shubhamshree@iVMBA-M2-09.localdomain>
2024-04-15 17:51:35 +0530

    fixed bugs

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,15 +14,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <head>
-        <link rel="preload" href="https://fonts.googleapis.com" />
-        <link rel="preload" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fjalla+One&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Trirong:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet preload"
+          rel="stylesheet"
         />
         <link
           href="https://db.onlinewebfonts.com/c/0a6ee448d1bd65c56f6cf256a7c6f20a?family=Bahnschrift"
-          rel="stylesheet preload"
+          rel="stylesheet"
         />
         <script
           rel="preload"
