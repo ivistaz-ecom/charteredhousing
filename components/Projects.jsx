35655d78bcb4d3a86324d4cf574044bc631d95fc
Shubham Shree <shubhamshree@iVMBA-M2-09.local>
2024-04-26 17:27:49 +0530

    Completed Project: bugs fixed for mobile view

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,16 +3,28 @@ import { Col } from 'react-bootstrap'
 const Projects = ({ projectDetails }) => {
   const { year, title, address, borderStyle } = projectDetails
   return (
-    <Col md={6} lg={4}>
-      <li className={`m-3 mb-4 p-3 ${borderStyle}`}>
-        <div className="me-4 mb-3 project-container text-center">
-          <h1 className="project-year">{year}</h1>
-          <p className="project-title">{title}</p>
-          <p className="project-add">{address}</p>
-        </div>
-      </li>
-      <div className="horizontal-line-red w-100" />
-    </Col>
+    <>
+      <Col md={6} lg={4} className="d-none d-md-block">
+        <li className={`m-3 mb-4 p-3 ${borderStyle}`}>
+          <div className="me-4 mb-3 project-container text-center">
+            <h1 className="project-year">{year}</h1>
+            <p className="project-title">{title}</p>
+            <p className="project-add">{address}</p>
+          </div>
+        </li>
+        <div className="horizontal-line-red w-100" />
+      </Col>
+      <Col md={6} lg={4} className="d-block d-md-none">
+        <li className={`m-3 mb-4 p-3`}>
+          <div className="me-4 mb-3 project-container text-center">
+            <h1 className="project-year">{year}</h1>
+            <p className="project-title">{title}</p>
+            <p className="project-add">{address}</p>
+          </div>
+        </li>
+        <div className="horizontal-line-red w-100" />
+      </Col>
+    </>
   )
 }
 
