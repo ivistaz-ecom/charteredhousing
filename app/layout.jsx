880eca478988b2286bad4dce7b83950df928fd37
Shubham Shree <shubhamshree@iVMBA-M2-09.localdomain>
2024-04-29 16:43:20 +0530

    home page: responsiveness

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ const RootLayout = ({ children }) => {
         <link
           rel="preload"
           href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
+          crossorigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fjalla+One&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Trirong:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -26,7 +26,7 @@ const RootLayout = ({ children }) => {
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-          crossOrigin="anonymous"
+          crossorigin="anonymous"
           defer
         ></Script>
       </head>
